e443989156abdac98a32739d8adf20e2f1d7f8aa
김보석 <boseoggim08@gmail.com>
2021-12-06 22:13:16 +0900

    Update 2042_Check_if_Numbers_Are_Ascending_in_a_Sentence.py

--- 2042_Check_if_Numbers_Are_Ascending_in_a_Sentence.py
+++ 2042_Check_if_Numbers_Are_Ascending_in_a_Sentence.py
@@ -1,6 +1,6 @@
 class Solution:
     def areNumbersAscending(self, s: str) -> bool:
         spl = [int(i) for i in s.split(' ') if i.isdigit()]
-        if list(dict.fromkeys(spl)) == spl :
+        if list(dict.fromkeys(spl)) == spl:
             return sorted(spl) == spl
         return False
